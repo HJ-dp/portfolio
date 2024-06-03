9342dd96188e1a6601919ffcd266aaf29a90ffde
이호진 <skwo777@naver.com>
2024-06-04 01:56:22 +0900

    refactor: 경로 변경

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import styles from "./MainPage.module.scss";
 function MainPage() {
   return (
     <div className={styles.mainContainer}>
-      <img src="src/assets/hojinlee.jpg" alt="hojin" />
+      <img src="../../src/assets/hojinlee.jpg" alt="hojin" />
     </div>
   );
 }
