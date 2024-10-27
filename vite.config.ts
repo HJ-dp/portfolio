268ddf32f0e814f4a02d2888a407204dc353d61e
이호진 <skwo777@naver.com>
2024-10-28 06:12:47 +0900

    build: .env, 전역변수 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    define: {
+      __IMAGE_URL__:
+        env.MODE === "production"
+          ? JSON.stringify(env.VITE_NGINX_IMAGE_URL)
+          : JSON.stringify(env.VITE_LOCAL_IMAGE_URL),
+    },
+  };
+});
