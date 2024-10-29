9daa1fede386928ad8b1333f55098d4342c17ef1
이호진 <skwo777@naver.com>
2024-10-30 03:08:10 +0900

    feat: GA4 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
+import ReactGA from "react-ga4";
 import { Global } from "@emotion/react";
 import { globalCss, globalFont } from "./styles/globalStyle";
+if (import.meta.env.VITE_GA4) {
+  ReactGA.initialize(import.meta.env.VITE_GA4);
+}
 
 const App = () => {
   return (
